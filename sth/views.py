6def6aacc668c062d1e63cd2636a87f168574805
Anymre <36187602+Anymre@users.noreply.github.com>
2019-09-30 15:25:48 +0800

    3d

--- sth/views.py
+++ sth/views.py
@@ -1,19 +1,33 @@
 import datetime
 import json
+import random
 from datetime import datetime, timedelta
 
+import math
 import requests
 from django.shortcuts import render
+from django.http import HttpResponse
+from pyecharts import options as opts
 
 # Create your views here.
 from django.template import loader
-from pyecharts.charts import Line
+from pyecharts.charts import Line, EffectScatter, Line3D, Scatter3D
+from pyecharts.faker import Faker
+from pyecharts.globals import ThemeType
 
 url = "http://server.anymre.top:8927/data"
 
 
+def index(request, mouth, day, hour, minute):
+    return HttpResponse(perform(mouth, day, hour, minute))
+
+
 def get_Date(r):
     return r["Date"]
+
+
+def get_Now(r):
+    return r["Now"]
 
 
 def format(r):
@@ -21,20 +35,69 @@ def format(r):
 
 
 def clean(r, mouth, day, hour, minute):
-    flag = True
+    flag0, flag1, flag2, flag3 = True, True, True, True
     if mouth != 99:
-        flag = r.month == mouth
+        flag0 = r.month == mouth
     if day != 99:
-        flag = r.day == day
+        flag1 = r.day == day
     if hour != 99:
-        flag = r.hour == hour
+        flag2 = r.hour == hour
     if minute != 99:
-        flag = r.minute == minute
-    return flag
+        flag3 = r.minute == minute
+    return flag0 and flag1 and flag2 and flag3
 
 
 def time_str(r):
+    return r.strftime("%m/%d %H:%M")
+
+
+def time_m_d(r):
     return r.strftime("%m/%d")
+
+
+def time_h_m(r):
+    return r.strftime("%H:%M")
+
+
+def time_h_m_int(r):
+    return r.hour * 100 + r.minute
+
+def sort_by_hour(r):
+    return time_h_m_int(get_Date(r))
+
+
+def chart(f):
+    x = [time_str(i["Now"]) for i in f]
+    y = [i["Price"] for i in f]
+    l = (
+        Line(init_opts=opts.InitOpts(theme=ThemeType.LIGHT, width="1366px"))
+            .add_xaxis(x)
+            .add_yaxis("v", y, is_connect_nones=True)
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title="MC"),
+            toolbox_opts=opts.ToolboxOpts(feature=opts.ToolBoxFeatureOpts()),
+            visualmap_opts=opts.VisualMapOpts(min_=500, max_=1200),
+        )
+    )
+    return l.render_embed()
+
+
+def scatter3d_base(f) -> Scatter3D:
+    f.sort(key=sort_by_hour)
+    data = [
+        [time_m_d(i["Now"]),time_h_m(i["Now"]), i["Price"]]
+        for i in f
+    ]
+    c = (
+        Scatter3D(init_opts=opts.InitOpts(theme=ThemeType.LIGHT, width="1366px"))
+            .add("", data)
+            .set_global_opts(
+            visualmap_opts=opts.VisualMapOpts(min_=300,max_=1200),
+            title_opts=opts.TitleOpts(title="MC"),
+            toolbox_opts=opts.ToolboxOpts(feature=opts.ToolBoxFeatureOpts()),
+        )
+    )
+    return c.render_embed()
 
 
 def perform(mouth, day, hour, minute):
@@ -43,24 +106,13 @@ def perform(mouth, day, hour, minute):
 
     f = r["Forward"]
     f.sort(key=get_Date)
+    f.sort(key=get_Now)
+
     for i in f:
         i["Date"] = format(i["Date"])
         i["Now"] = format(i["Now"])
 
     f = [i for i in f if clean(i["Date"], mouth, day, hour, minute)]
 
-    x = [time_str(i["Date"]) for i in f]
-    y = [i["Price"] for i in f]
-
-    l = Line()
-    l.add_xaxis(x)
-    l.add_yaxis("v", y)
-    l.width = 768
-    return l.render_embed()
-
-
-from django.http import HttpResponse
-
-
-def index(request, mouth, day, hour, minute):
-    return HttpResponse(perform(mouth, day, hour, minute))
+    # return chart(f)
+    return scatter3d_base(f)
